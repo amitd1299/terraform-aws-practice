a1fcebd7f42e9ec2b97e677f5c814b20a0f2e797
amitd1299 <dorwekar2000@gmail.com>
2026-07-12 15:28:20 +0530

    Add Remote State - S3 backend + DynamoDB lock

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "amit-terraform-state-564186"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
