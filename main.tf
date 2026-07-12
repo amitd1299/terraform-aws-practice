92e2f5df4a1127685e8d427c3ccf75d30461bc8e
amitd1299 <dorwekar2000@gmail.com>
2026-07-12 15:43:10 +0530

    Add Security Group - SSH and HTTP rules

--- main.tf
+++ main.tf
@@ -26,13 +26,47 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "myserver" {
-  ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.mysg.id]
 
   tags = {
     Name = "${var.project_name}-server"
+  }
+}
+# Security Group
+resource "aws_security_group" "mysg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ tags = {
+    Name = "${var.project_name}-sg"
   }
 }
